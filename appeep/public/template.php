7167ef8181321e3d0e04396958eb20da39d1a057
osamuya <oosamuuy@gmail.com>
2017-09-03 15:57:17 +0900

    画像サムネイルの調整

--- appeep/public/template.php
+++ appeep/public/template.php
@@ -36,6 +36,11 @@
 					<a href="#"><img src="/assets/img/dummy03.jpg"></a>
 					<a href="#"><img src="/assets/img/dummy04.jpg"></a>
 					<a href="#"><img src="/assets/img/dummy05.jpg"></a>
+					<a href="#"><img src="/assets/img/dummy01.jpg"></a>
+					<a href="#"><img src="https://placehold.jp/320x320.png"></a>
+					<a href="#"><img src="/assets/img/dummy03.jpg"></a>
+					<a href="#"><img src="https://placehold.jp/100x150.png"></a>
+					<a href="#"><img src="https://placehold.jp/150x150.png"></a>
 				</div>
 			</div>
 			
